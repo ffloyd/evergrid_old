9723598b8d8f08fb9a8d4c3a1509e73b16fc38cd
Roman Kolesnev <rvkolesnev@gmail.com>
2015-11-25 17:35:01 +0300

    Simple github login

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,10 @@
 App = React.createClass({
   render() {
     return (
-      <h1>Hello from React!</h1>
+      <div>
+        <h1>Hello from React!</h1>
+        <LoginButtons/>
+      </div>
     );
   },
 });
